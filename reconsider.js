8a4e9ff86732e015101eb37c1a2925af0bf93d72
Andrew Liu <17liut@gmail.com>
2018-05-05 15:21:39 -0400

    Connect to Google Language API

--- reconsider.js
+++ reconsider.js
@@ -1,3 +1,20 @@
 function reconsider(text) {
-  alert(text);
+  analyze(text)
+    .then(res => res.json())
+    .then(data => alert(JSON.stringify(data)));
+}
+
+function analyze(text) {
+  return fetch('https://language.googleapis.com/v1/documents:analyzeSentiment?key=AIzaSyBu7B2xRdd3jFoA1sh4qI0ULkkhthvQPEs', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      document: {
+        content: text,
+        type: 'PLAIN_TEXT'
+      }
+    }),
+  });
 }
