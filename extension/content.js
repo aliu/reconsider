2fde05d457e292d8447447ed61523f389ab1f916
Andrew Liu <17liut@gmail.com>
2018-05-06 02:15:48 -0400

    Add Facebook extension

---
+++ extension/content.js
@@ -0,0 +1,18 @@
+document.body.onkeydown = function(e) {
+  try {
+    var message = e.target.firstChild.firstChild.innerText;
+    if (e.key === 'Enter' && !e.flag) {
+      e.preventDefault();
+      e.stopPropagation();
+      reconsider(message, function(wait) {
+        if (wait) {
+          console.log('Wait! Please reconsider sending this message!');
+        } else {
+          e.flag = true;
+          e.target.dispatchEvent(e);
+        }
+      });
+    }
+  } catch (error) {
+  }
+};
---
+++ extension/reconsider.js
@@ -0,0 +1,28 @@
+function reconsider(text, callback) {
+  analyze(text)
+    .then(res => res.json())
+    .then(data => {
+      for (sentence of data.sentences) {
+        if (sentence.sentiment.score <= -0.5 && sentence.sentiment.magnitude >= 0.5) {
+          callback(true);
+          return;
+        }
+      }
+      callback(false);
+    });
+}
+
+function analyze(text) {
+  return fetch('https://language.googleapis.com/v1/documents:analyzeSentiment?key=AIzaSyBu7B2xRdd3jFoA1sh4qI0ULkkhthvQPEs', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      document: {
+        content: text,
+        type: 'PLAIN_TEXT'
+      }
+    }),
+  });
+}
